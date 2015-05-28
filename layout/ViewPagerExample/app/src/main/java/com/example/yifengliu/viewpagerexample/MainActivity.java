251c03eb748a7696914691993428bde882682c86
liuyifeng <lyfshadyboss@gmail.com>
2015-05-28 15:36:26 +0800

    added ViewPagerExample

---
+++ layout/ViewPagerExample/app/src/main/java/com/example/yifengliu/viewpagerexample/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.yifengliu.viewpagerexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity implements ViewPager.OnPageChangeListener {
+
+    private ViewPager viewPager;
+    private List<View> views = new ArrayList<>();
+
+    private LinearLayout container;
+    private LayoutInflater inflater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inflater = getLayoutInflater();
+        container = (LinearLayout) findViewById(R.id.container);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        views = new ArrayList<>();
+        for (int i = 1; i <= 12; i++) {
+            View view = inflater.inflate(R.layout.item, null);
+            views.add(view);
+        }
+
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setPageMargin(20);
+
+        // 将父类的touch事件分发至viewPgaer，否则只能滑动中间的一个view对象
+        container.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return viewPager.dispatchTouchEvent(event);
+            }
+        });
+
+        viewPager.setAdapter(new MyAdapter());
+        viewPager.setOnPageChangeListener(this);
+    }
+
+    class MyAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return views.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = views.get(position);
+            container.addView(view);
+            return views.get(position);
+        }
+    }
+
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    public void onPageSelected(int position) {
+    }
+
+    /*
+     * SCROLL_STATE_IDLE: pager处于空闲状态 SCROLL_STATE_DRAGGING： pager处于正在拖拽中
+     * SCROLL_STATE_SETTLING： pager正在自动沉降，相当于松手后，pager恢复到一个完整pager的过程
+     */
+    public void onPageScrollStateChanged(int state) {
+    }
+}
