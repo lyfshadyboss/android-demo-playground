71e04640b45f6d1755c23198746cacdc6fb00cd4
yifengliu <yifengliu@Ctrip.com>
2015-07-02 17:05:32 +0800

    added material animations

---
+++ animation/Material-Animations/app/src/main/java/com/example/yifengliu/material_animations/DetailActivity1.java
@@ -0,0 +1,24 @@
+package com.example.yifengliu.material_animations;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.transition.Explode;
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class DetailActivity1 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details1);
+        setupWindowAnimations();
+    }
+
+    private void setupWindowAnimations() {
+        Explode explode = new Explode();
+        explode.setDuration(2000);
+        getWindow().setEnterTransition(explode);
+    }
+}
